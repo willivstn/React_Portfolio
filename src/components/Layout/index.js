e76ac6cff8e6a46022a2a9626b9ba1e1de849b6e
Willie van Staden <60608218+willivstn@users.noreply.github.com>
2023-02-01 22:01:29 +0200

    Initial commit

---
+++ src/components/Layout/index.js
@@ -0,0 +1,26 @@
+import './index.scss';
+import Sidebar from '../Sidebar/'
+import { Outlet } from 'react-router-dom';
+
+const Layout = () => {
+    return (
+        <div className = "App">
+            <Sidebar/>
+            <div className='page'>
+            <span className='tag top-tags'>&lt;body&gt;</span>
+
+             <Outlet/>
+
+             <span className='tags bottom-tags'>
+
+             </span>
+
+         </div>
+        </div>
+    
+    )
+}
+    
+
+
+export default Layout
---
+++ src/components/Sidebar/index.js
@@ -0,0 +1,53 @@
+import {Link, NavLink} from 'react-router-dom'
+import './index.scss'
+import LogoS from '../../assets/images/logo-s.png'
+import LogoSubtitle from '../../assets/images/logo_sub.png'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEnvelope, faHome, faUser } from '@fortawesome/free-solid-svg-icons'
+import { faGithub, faLinkedin, faYoutube } from '@fortawesome/free-brands-svg-icons'
+
+
+const Sidebar = () => (
+    <div className='nav-bar'>
+        <Link className='logo' to='/'>
+            <img src={LogoS} alt ="logo"/>
+            <img className='sub-logo' src={LogoSubtitle} alt ="slobodan"/>
+
+        </Link>
+
+        <nav>
+            <NavLink exact="true" activeclassname="active" to="/">
+                <FontAwesomeIcon icon={faHome} color="#4d4d4e"/>
+            </NavLink>
+
+            <NavLink exact="true" activeclassname="active" className="about-link" to="/about">
+                <FontAwesomeIcon icon={faUser} color="#4d4d4e"/>
+            </NavLink>
+
+            <NavLink exact="true" activeclassname="active" className="contact-link" to="/contact">
+                <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e"/>
+            </NavLink>
+        </nav>
+        <ul>
+            <li>
+                <a target="_blank" rel='noreferrer' href='https://www.linkedin.com/in/willie-van-staden-130a741a9/'>
+                    <FontAwesomeIcon icon={faLinkedin} color="#4d4d4e"/>
+                </a>
+            </li>
+            <li>
+                <a target="_blank" rel='noreferrer' href='https://github.com/willivstn'>
+                    <FontAwesomeIcon icon={faGithub} color="#4d4d4e"/>
+                </a>
+            </li>
+            <li>
+                <a target="_blank" rel='noreferrer' href='https://www.youtube.com/'>
+                    <FontAwesomeIcon icon={faYoutube} color="#4d4d4e"/>
+                </a>
+            </li>
+            
+        </ul>
+
+    </div>
+)
+
+export default Sidebar
